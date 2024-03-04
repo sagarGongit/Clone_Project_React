daaf897ded00182d42f661fdcfa865c7fb7b3983
Sagar Bhagwatkar <148358555+sagarGongit@users.noreply.github.com>
2024-03-04 23:26:27 +0530

    Update App.js

--- allegroclone/src/App.js
+++ allegroclone/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import React from 'react';
 import HomePage from './Pages/Homepage';
-import AllRoute from './AllRoutes/AllRoute';
 
 function App() {
 
   return (
     
        <div >
-        <AllRoute/>
        <HomePage/>
     </div>
   ) 
